65fc0264d2550edaede733c4dffedb020d57ec85
Freddie Haddad <freddie.haddad@outlook.com>
2025-05-12 07:08:44 -0700

    Add database to store all known cpp file locations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use anyhow::{Context, Result};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use std::fs::File;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
 use std::io::{BufRead, BufReader, Write};
+use std::{fs, fs::File};
 use std::{path::Path, path::PathBuf};
 
 #[derive(Deserialize, Serialize)]
@@ -19,15 +21,23 @@ struct CompileCommand {
 )]
 struct Cli {
     /// Path to msbuild.log
-    #[arg(short, long)]
+    #[arg(short('i'), long)]
     input_file: PathBuf,
 
     /// Output JSON file
-    #[arg(short, long, default_value = "compile_commands.json")]
+    #[arg(short('o'), long, default_value = "compile_commands.json")]
     output_file: PathBuf,
 
+    /// Path to source code
+    #[arg(short('d'), long, default_value = r"S:\Azure\Storage\XStore")]
+    source_directory: PathBuf,
+
+    /// File extension for cpp files
+    #[arg(short('e'), long, default_value = "cpp")]
+    source_extension: PathBuf,
+
     /// Name of compiler executable
-    #[arg(short, long, name = "EXE", default_value = "cl.exe")]
+    #[arg(short('c'), long, name = "EXE", default_value = "cl.exe")]
     compiler_executable: String,
 }
 
@@ -41,7 +51,7 @@ fn filter_compile_commands(
         .map_while(Result::ok)
         .filter_map(|line| {
             if line.to_lowercase().contains(&filter) {
-                Some(String::from(line.trim()))
+                Some(line.replace("\"", ""))
             } else {
                 None
             }
@@ -61,6 +71,7 @@ fn get_target_cpp_file(arguments: &[String]) -> Result<&String> {
 /// Converts a vector of compile commands into a CompileCommand.
 fn generate_entries(
     compile_commands: Vec<String>,
+    directory_tree: HashMap<String, String>,
 ) -> Result<Vec<CompileCommand>> {
     let mut entries = Vec::new();
     for compile_command in &compile_commands {
@@ -73,27 +84,6 @@ fn generate_entries(
         // command.
         // Example: S:\Azure\Storage\XStore\src\base\PlatformConfig\lib\vdsutils.cpp
         let target_cpp_file = Path::new(get_target_cpp_file(&arguments)?);
-
-        // The directory field of the compile_commands.json entry
-        // S:\Azure\Storage\XStore\src\base\PlatformConfig\lib\
-        let directory = match target_cpp_file.parent() {
-            Some(parent) => parent.display().to_string(),
-            None => {
-                eprintln!(
-                    "Unable to extract parent component: {}",
-                    target_cpp_file.display()
-                );
-                continue;
-            }
-        };
-
-        if directory.is_empty() {
-            eprintln!(
-                "Parent component is empty: {}",
-                target_cpp_file.display()
-            );
-            continue;
-        }
 
         // The file field of the compile_commands.json entry
         // vdsutils.cpp
@@ -116,6 +106,42 @@ fn generate_entries(
             continue;
         }
 
+        // The directory field of the compile_commands.json entry
+        // S:\Azure\Storage\XStore\src\base\PlatformConfig\lib\
+        let directory = match target_cpp_file.parent() {
+            Some(parent) => parent.display().to_string(),
+            None => {
+                eprintln!("{}: parent unknown", target_cpp_file.display());
+                String::new()
+            }
+        };
+
+        let directory = match directory.is_empty() {
+            false => directory,
+            true => match directory_tree.get(&file_name) {
+                Some(dir) => {
+                    if dir.is_empty() {
+                        eprintln!(
+                            "{}: no entry found in tree",
+                            target_cpp_file.display()
+                        );
+                        continue;
+                    } else {
+                        dir.clone()
+                    }
+                }
+                None => {
+                    eprintln!(
+                        "{}: duplicate entries found in tree",
+                        target_cpp_file.display()
+                    );
+                    continue;
+                }
+            },
+        };
+
+        assert!(!directory.is_empty());
+
         // Construct and add the entry
         entries.push(CompileCommand {
             file: file_name,
@@ -126,6 +152,54 @@ fn generate_entries(
     Ok(entries)
 }
 
+/// Explores the entire directory tree starting from `dir` adding any files with `extension` to the
+/// `tree` as the key and the parent path of the file as the value.  This lookup table is used for
+/// adding the directory entry to the compile_commands.json file where its not specified on the
+/// command line.
+///
+/// Because files with matching names can exist in multiple directories, these cases result in the
+/// value entry in the tree being set to the empty string since we cannot know which path is the
+/// correct one.
+fn find_files(
+    dir: &Path,
+    extension: &Path,
+    tree: &mut HashMap<String, String>,
+) -> Result<()> {
+    // Iterate over each file in dir
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+
+        // Further explore any directory
+        if path.is_dir() {
+            find_files(&path, extension, tree)?;
+            continue;
+        }
+
+        // Test if non-directory entry is a file with a matchin extension
+        if let Some(ext) = path.extension() {
+            if ext.len() != 3 || ext.to_ascii_lowercase() != extension {
+                continue;
+            }
+
+            let file_name =
+                String::from(path.file_name().unwrap().to_string_lossy());
+            let parent = String::from(path.parent().unwrap().to_string_lossy());
+
+            // Add key/value pair (file_name/path) to the hash table or clear on collision
+            match tree.entry(file_name) {
+                Entry::Vacant(e) => {
+                    e.insert(parent);
+                }
+                Entry::Occupied(mut e) => {
+                    e.get_mut().clear();
+                }
+            };
+        }
+    }
+    Ok(())
+}
+
 fn main() -> Result<()> {
     // Parse command line arguments
     let cli = Cli::parse();
@@ -134,6 +208,8 @@ fn main() -> Result<()> {
     let input_file = cli.input_file;
     let output_file = cli.output_file;
     let compiler_executable = cli.compiler_executable;
+    let source_directory = cli.source_directory;
+    let source_extension = cli.source_extension;
 
     // File reader
     let input_file_handle = File::open(&input_file).with_context(|| {
@@ -152,6 +228,18 @@ fn main() -> Result<()> {
             format!("Failed to open {}", output_file.to_string_lossy())
         })?;
 
+    // Build directory tree
+    anyhow::ensure!(
+        source_directory.is_dir(),
+        format!(
+            "Provided path is not a directory: {}",
+            source_directory.display()
+        )
+    );
+
+    let mut source_tree: HashMap<String, String> = HashMap::new();
+    find_files(&source_directory, &source_extension, &mut source_tree)?;
+
     // Collect all the compile commands from the input file
     let compile_commands: Vec<_> =
         filter_compile_commands(input_file_handle, compiler_executable);
@@ -159,7 +247,7 @@ fn main() -> Result<()> {
     println!("Found {} compile commands", compile_commands.len());
 
     // Tokenize the compile commands
-    let compile_commands = generate_entries(compile_commands)?;
+    let compile_commands = generate_entries(compile_commands, source_tree)?;
 
     // Generate the compile_commands.json file
     let serialized = serde_json::to_string_pretty(&compile_commands)?;
